3cc68bfd3db1b9c1e5c2ec6a91f50942c26d7ac1
dustbox <sanggae4133@naver.com>
2024-11-02 17:11:56 +0900

    first commit

---
+++ src/test/java/com/roomie/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomie.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
