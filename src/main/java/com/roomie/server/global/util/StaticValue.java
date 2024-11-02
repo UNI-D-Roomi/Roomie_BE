2abe1a0893b276ead8fbf730446a5056eca7f588
dustbox <sanggae4133@naver.com>
2024-11-03 02:37:43 +0900

    feat: 점수 컷오프 수정

--- src/main/java/com/roomie/server/global/util/StaticValue.java
+++ src/main/java/com/roomie/server/global/util/StaticValue.java
@@ -4,7 +4,7 @@ public class StaticValue {
 
     public static final Long HUNGER_MINUS_SECOND_STRIDE = 10L;
 
-    public static final Double SCORE_CUT_OFF = 50.0;
+    public static final Double SCORE_CUT_OFF = 30.0;
     public static final String ROOM_FEED_COMMENT = "깨끗한 방 청소로 루미가 배부르게 먹었어요!";
     public static final String WASH_DISHES_FEED_COMMENT = "깨끗한 설거지로 루미가 배부르게 먹었어요!";
     public static final Integer RIBBON_COST = 200;
